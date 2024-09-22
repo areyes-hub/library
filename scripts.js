7956ac3fc2709e6071451a7a4a7afe65177338f3
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-09-22 16:42:28 -0400

    Resolve delete functionality issue

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 let myLibrary = [];
 
-let board;
 let template;
 let rmvBtn;
+let update;
 
 let form = document.querySelector("form");
 let clearBtn = document.querySelector(".clear");
 
 let container = document.querySelector(".container");
 let rightSide = document.querySelector(".right");
+let leftSide = document.querySelector(".left");
 let lib = document.querySelector(".lib")
 let display = document.querySelector(".center");
 let bottom = document.querySelector(".bottom");
@@ -16,9 +17,11 @@ let view = document.querySelector(".view");
 
 let add = document.querySelector(".add-new");
 add.addEventListener("click", () => {
-    container.style.gridTemplateColumns = "40% 60%";
+    container.style.gridTemplateColumns = "39% 61%";
     display.style.backgroundImage = "none";
     display.style.opacity = 1;
+    rightSide.style.width = "100%";
+    leftSide.style.width = "100%";
     lib.textContent = "Books";
     add.style.display = "none";
 });
@@ -35,15 +38,15 @@ function Book(title, author, year, pages) {
         this.pages = pages.value
 }
 
-function createLib(arr) {
+function createTab(arr) {
     for (let i = 0; i <= arr.length - 1; i++) {
         template = document.createElement("div");
         rmvBtn = document.createElement("button");
-        template.classList.add(`book-${i}`);
+        template.classList.add(`book${i}`);
         template.style.display = "flex";
         template.style.justifyItems = "center";
         template.style.alignItems = "center";
-        template.style.width = "85%";
+        template.style.width = "75%";
         template.style.padding = "8px";
         template.style.marginBottom = "8px";
         template.style.backgroundColor = "lightskyblue";
@@ -53,7 +56,11 @@ function createLib(arr) {
         rmvBtn.style.padding = "4px";
         rmvBtn.style.marginLeft = "auto";
         rmvBtn.style.borderRadius = "8px";
-        rmvBtn.setAttribute("onclick", "deleteParent(this)")
+        rmvBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            arr.splice(i, 1);
+        })
+        rmvBtn.setAttribute("onclick", "deleteParent(this)");
         display.appendChild(template);
         template.appendChild(rmvBtn);
     }
@@ -66,7 +73,10 @@ function deleteParent(button) {
 function viewAll() {
     view.addEventListener("click", (event) => {
         event.preventDefault();
-        createLib(myLibrary);
+        update = document.createElement("h2");
+        update.textContent = "Books Available!";
+        display.appendChild(update);
+        createTab(myLibrary);
     })
 }
 
@@ -74,7 +84,7 @@ function addBookToLibrary() {
     form.addEventListener("submit", (event) => {
         event.preventDefault();
         let book = new Book(title, author, year, pages);
-        myLibrary.push(`Title: ${book.title}, author: ${book.author}, year: ${book.year}, pages: ${book.pages};`);
+        myLibrary.push(`Title: ${book.title}, author: ${book.author}, year: ${book.year}, pages: ${book.pages}`);
         form.reset();
     })
 }
@@ -82,6 +92,5 @@ function addBookToLibrary() {
 
 addBookToLibrary();
 viewAll();
-
 
 
